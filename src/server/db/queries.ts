0d1b02e8473689635017310863fda72faecdd332
Salomon Romano <salo_romano@yahoo.com>
2025-02-25 16:15:00 -0600

    Ensure consistent sort order for folders and files in database queries

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { eq } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 import { db } from "~/server/db";
 import {
@@ -31,13 +31,15 @@ export const QUERIES = {
     return db
       .select()
       .from(foldersSchema)
-      .where(eq(foldersSchema.parent, folderId));
+      .where(eq(foldersSchema.parent, folderId))
+      .orderBy(asc(foldersSchema.name));
   },
   getFiles: function (folderId: number) {
     return db
       .select()
       .from(filesSchema)
-      .where(eq(filesSchema.parent, folderId));
+      .where(eq(filesSchema.parent, folderId))
+      .orderBy(asc(filesSchema.name));
   },
   getFolderById: async function (folderId: number) {
     const folder = await db
